f0c1bf96549cf79fc4f9c4d728d5578b2d98dfbf
Lizzy <internelisabeth@gmail.com>
2017-04-02 19:49:12 -0400

    Update fahrenheit.c

--- fahrenheit.c
+++ fahrenheit.c
@@ -1,12 +1,12 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    float c, f = 0.0;
+    float celsius = 0.0, f = 0.0;
     printf("C: ");
-    scanf("%f", &c);
-    if (c )
-    f = ((c * 9) / 5) + 32;
+    scanf("%f", &celsius);
+    f = (((celsius * 9) / 5) + 32);
     printf("F: %.1f\n", f);
     return 0;
 }
